58e43da8b5a1a8cf6bc1a620096c9533a046db35
roblivesinottawa <tech.rob@icloud.com>
2021-02-09 19:23:49 -0500

    "need to add game over and refactor code"

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import os
 import random
+import time
+
 import turtle
-
-
 turtle.fd(0)
 turtle.speed(0)
 turtle.bgcolor("black")
+# add background image
+turtle.bgpic("starfield.gif")
+# change window title
+turtle.title("Space War")
 turtle.hideturtle()
 turtle.setundobuffer(1)
-turtle.tracer(1)
+turtle.tracer(0)
 
 
 class Sprite(turtle.Turtle):
@@ -51,6 +55,7 @@ class Sprite(turtle.Turtle):
 class Player(Sprite):
     def __init__(self, spriteshape, color, startx, starty):
         Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.shapesize(stretch_wid=0.6, stretch_len=1.1, outline=None)
         self.speed = 4
         self.lives = 3
 
@@ -100,13 +105,16 @@ class Ally(Sprite):
 class Missile(Sprite):
     def __init__(self, spriteshape, color, startx, starty):
         Sprite.__init__(self, spriteshape, color, startx, starty)
-        self.shapesize(stretch_wid=0.3, stretch_len=0.4, outline=None)
+        self.shapesize(stretch_wid=0.2, stretch_len=0.4, outline=None)
         self.speed = 20
         self.status = "ready"
         self.goto(-1000, 1000)
 
     def fire(self):
         if self.status == "ready":
+            # play missile sound
+            # os.system("afplay laser.mp3&")
+
             self.goto(player.xcor(), player.ycor())
             self.setheading(player.heading())
             self.status = "firing"
@@ -125,6 +133,26 @@ class Missile(Sprite):
             self.goto(-1000, 1000)
             self.status = "ready"
 
+class Particle(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.shapesize(stretch_wid=0.1, stretch_len=0.1, outline=None)
+        self.goto(-1000, -1000)
+        self.frame = 0
+
+    def explode(self, startx, starty):
+        self.got(startx, starty)
+        self.setheading(random.randint(0, 360))
+        self.frame = 1
+
+    def move(self):
+        if self.frame > 0:
+            self.fd(10)
+            self.frame += 1
+
+        if self.frame < 15:
+            self.frame = 0
+            self.goto(-1000, -1000)
 
 
 class Game():
@@ -147,6 +175,14 @@ class Game():
             self.pen.right(90)
         self.pen.penup()
         self.pen.hideturtle()
+        self.pen.pendown()
+
+    def show_status(self):
+        self.pen.undo()
+        msg = f"Score: {self.score}"
+        self.pen.penup()
+        self.pen.goto(-300, 310)
+        self.pen.write(msg, font=("Arial",16, "normal"))
 
 # create game object
 game = Game()
@@ -154,11 +190,27 @@ game = Game()
 # draw the border
 game.draw_border()
 
+# show the game status
+
+game.show_status()
+
 # create sprites
 player = Player("triangle", "white", 0, 0)
-enemy = Enemy("circle", "red", -100, 0)
+# enemy = Enemy("circle", "red", -100, 0)
 missile = Missile("triangle", "yellow", 0, 0)
-ally = Ally("square", "blue", 0, 0)
+# ally = Ally("square", "blue", 100, 0)
+
+enemies = []
+for e in range(6):
+    enemies.append(Enemy("circle", "red", -100, 0))
+
+allies = []
+for a in range(6):
+    enemies.append(Ally("square", "blue", 100, 0))
+
+particles = []
+for i in range(20):
+    particles.append(Particle("circle", "orange", 0, 0))
 
 # keyboard bindings
 turtle.onkey(player.turn_left, "Left")
@@ -173,33 +225,70 @@ turtle.listen()
 
 # main game loop
 while True:
+    turtle.update()
+    time.sleep(0.02)
     player.move()
-    enemy.move()
+    # enemy.move()
     missile.move()
-    ally.move()
+    # ally.move()
 
-    # check for collision with player
-    if player.is_collision(enemy):
-        x = random.randint(-250, 250)
-        y = random.randint(-250, 250)
-        enemy.goto(x, y)
+    for enemy in enemies:
+        enemy.move()
 
-    # check for collision between missile and enemy
-    if missile.is_collision(enemy):
-        x = random.randint(-250, 250)
-        y = random.randint(-250, 250)
-        enemy.goto(x, y)
-        missile.status = "ready"
+        # check for collision with player
+        if player.is_collision(enemy):
+            # play explosion sound
+            # os.system("afplay explosion.mp3&")
+            x = random.randint(-250, 250)
+            y = random.randint(-250, 250) 
+            enemy.goto(x, y)
+            game.score -= 100
+            game.show_status()
+       
+        if missile.is_collision(enemy):
+            # play missile sound
+            # os.system("afplay explosion.mp3&")
+            x = random.randint(-250, 250)
+            y = random.randint(-250, 250)
+            enemy.goto(x, y)
+            missile.status = "ready"
+            # increase score
+            game.score += 100
+            game.show_status()
+            # do the explosion
+            for particle in particles:
+                particle.explode(missile.xcor(), missile.ycor())
+                
+
+    for ally in allies:
+        ally.move()
+        
+        # check for collision between missile and ally
+        if missile.is_collision(ally):
+            # play missile sound
+            # os.system("afplay explosion.mp3&")
+            x = random.randint(-250, 250)
+            y = random.randint(-250, 250)
+            ally.goto(x, y)
+            missile.status = "ready"
+            # decrease the score
+            game.score -= 50
+            game.show_status()
+            
+    for particle in particles:
+        particle.move()
+           
+               
+
+  
+
+
+  
     
-    # check for collision between missile and ally
-    if missile.is_collision(ally):
-        x = random.randint(-250, 250)
-        y = random.randint(-250, 250)
-        ally.goto(x, y)
-        missile.status = "ready"
+    
    
 
 
 
-delay = input("press enter to finish > ")
+turtle.exitonclick()
 
